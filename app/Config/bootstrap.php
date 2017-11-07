c706696603dab09e8c218c0b206c0cd1f1e541ee
Tokunaga <tokunaga@gmail.com>
2017-11-07 11:07:40 +0000

    tmpの権限エラー修正、migrationプラグイン導入

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -23,7 +23,10 @@
  */
 
 // Setup a 'default' cache configuration for use in the application.
-Cache::config('default', array('engine' => 'File'));
+Cache::config('default', array(
+    'engine' => 'File',
+    'mask' => 0666
+));
 
 /**
  * The settings below can be used to set additional paths to models, views and controllers.
@@ -111,3 +114,4 @@ CakeLog::config('error', array(
 ));
 
 CakePlugin::load('DebugKit');
+CakePlugin::load('Migrations');
